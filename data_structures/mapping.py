e4e724ed985bf61947373cf690f53683e095a84b
ulhaqi12 <ulhaqi12@gmail.com>
2024-05-11 22:32:00 +0200

    first part done

--- data_structures/mapping.py
+++ data_structures/mapping.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from .ct_image import CTImage
 from .segmentation_image import SegmentationImage
@@ -8,6 +9,7 @@ class CTMapping:
 
     def __init__(self, ct_image: CTImage, segmentation_image: SegmentationImage):
         self.image = None
+        self.segmentation_image_combined = None
         self.segmentations = []
         self.ct_image = ct_image
         self.segmentation_image = segmentation_image
@@ -36,4 +38,92 @@ class CTMapping:
 
             self.segmentations.append(np.array(segmentation_image_slices))
 
+        self.combine_segmentations()
 
+    def plot_image_slice(self, slice_index, axis=0):
+        """
+        Plots a single slice from a 3D numpy array.
+
+        Parameters:
+        - data: 3D numpy array
+        - slice_index: index of the slice to be plotted
+        - axis: axis along which to take the slice (0, 1, or 2)
+        """
+        if axis == 0:
+            slice_data = self.image[slice_index, :, :]
+        elif axis == 1:
+            slice_data = self.image[:, slice_index, :]
+        elif axis == 2:
+            slice_data = self.image[:, :, slice_index]
+        else:
+            raise ValueError("Axis must be 0, 1, or 2.")
+
+        plt.imshow(slice_data, cmap='gray')
+        plt.colorbar()
+        plt.title(f'Slice {slice_index} along axis {axis}')
+        plt.show()
+
+    def plot_image_slice_with_segments(self, slice_index, axis=0):
+        """
+        Plots a single slice from multiple 3D numpy arrays in a 2x3 subplot layout.
+
+        Parameters:
+        - datas: list of 3D numpy arrays
+        - slice_index: index of the slice to be plotted
+        - axis: axis along which to take the slice (0, 1, or 2)
+        """
+        fig, axs = plt.subplots(2, 3, figsize=(15, 10))  # Set up a 2x3 subplot grid
+
+        all_figures = [self.image] + self.segmentations
+
+        for i, data in enumerate(all_figures):
+            ax = axs[i // 3, i % 3]  # Determine subplot location
+            if axis == 0:
+                slice_data = data[slice_index, :, :]
+            elif axis == 1:
+                slice_data = data[:, slice_index, :]
+            elif axis == 2:
+                slice_data = data[:, :, slice_index]
+            else:
+                raise ValueError("Axis must be 0, 1, or 2.")
+
+            if i == 0:
+                ax.imshow(slice_data, cmap='gray')
+                ax.set_title(f'Slice {slice_index} of Image {i + 1}')
+            else:
+                ax.imshow(slice_data, cmap='jet')
+                ax.set_title(f'Slice {slice_index} of Segmentation {i}')
+
+            ax.axis('off')  # Hide axes ticks
+
+        # Hide the last subplot as it's unused
+        axs[1, 2].axis('off')
+
+        plt.tight_layout()
+        plt.show()
+
+    def combine_segmentations(self, weights=None):
+        """
+        Computes a weighted sum of the entire 3D volumes from a list of 4 3D arrays based on given weights.
+
+        Parameters:
+        - segmentation_arrays: list of 4 3D numpy arrays.
+        - weights: list of floats, weights for each array. Default is [0.2, 0.4, 0.6, 0.9].
+
+        Returns:
+        - A 3D numpy array representing the weighted sum of the entire volumes.
+        """
+        if weights is None:
+            weights = [0.2, 0.4, 0.6, 0.9]  # Default weights
+
+        if len(self.segmentations) != 4 or len(weights) != 4:
+            raise ValueError("There must be exactly four segmentation arrays and four weights.")
+
+        # Initialize the result volume with a float type to accommodate weighted values
+        result_volume = np.zeros_like(self.segmentations[0], dtype=np.float64)
+
+        # Compute the weighted sum of the volumes
+        for seg_array, weight in zip(self.segmentations, weights):
+            result_volume += weight * seg_array
+
+        self.segmentation_image_combined = result_volume
--- utils.py
+++ utils.py
@@ -6,6 +6,9 @@ Author(s): Ikram Ul Haq (ulhaqi12)
 
 import pydicom
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import scipy
 
 
 def load_and_print_info(dicom_path):
@@ -60,4 +63,26 @@ def rotate_on_axial_plane(img_dcm: np.ndarray, angle_in_degrees: float) -> np.nd
     # Your code here:
     #   See `scipy.ndimage.rotate(...)`
     # ...
-    return scipy.ndimage.rotate(img_dcm, angle_in_degrees, axes=(1, 2))
+    return scipy.ndimage.rotate(img_dcm, angle_in_degrees, axes=(1, 2), reshape=False, order=0)
+
+
+def apply_cmap(img: np.ndarray, cmap_name: str = 'bone') -> np.ndarray:
+    """ Apply a colormap to a 2D image. """
+    cmap_function = matplotlib.colormaps[cmap_name]
+    return cmap_function(img)
+
+
+def visualize_alpha_fusion(img: np.ndarray, mask: np.ndarray, alpha: float = 0.25, aspect=1, visualize=False):
+    """ Visualize both image and mask in the same plot. """
+    img_sagittal_cmapped = apply_cmap(img, cmap_name='bone')    # Why 'bone'?
+    mask_bone_cmapped = apply_cmap(mask, cmap_name='prism')     # Why 'prism'?
+    mask_bone_cmapped = mask_bone_cmapped * mask[..., np.newaxis].astype('bool')
+
+    result = img_sagittal_cmapped * (1-alpha) + mask_bone_cmapped * alpha
+    if visualize:
+        plt.imshow(result, aspect=aspect)
+        plt.title(f'Segmentation with alpha {alpha}')
+        plt.show()
+    else:
+        return result
+
